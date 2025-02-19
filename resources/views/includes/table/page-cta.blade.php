8ae825f8d8fbab8d55ff2d29e25964aa5c3497eb
Py Sosu <prospersosu@gmail.com>
2025-02-19 06:33:09 +0100

    Temp fix for mobile more cta and desktop page cta

--- resources/views/includes/table/page-cta.blade.php
+++ resources/views/includes/table/page-cta.blade.php
@@ -25,7 +25,53 @@
     }
 }" class="page-cta-wrapper">
 
-    <div class="page-cta-wrapper-items mobile-cta" :style="{display: canShow}">
+    <div class="page-cta-wrapper-items mobile-cta for-desktop">
+        @foreach ($this->pageCta() as $key => $cta)
+            @if (isset($cta['show']) && $cta['show'])
+                @if (isset($cta['type']))
+                    @if ($cta['type'] === 'link')
+
+                        @if ($cta['withSidePanel'])
+                        <button type="button" class="custom-btn as-pointer {{$cta['css'] ?? ''}}"
+                            x-on:click="toggleSidePanel('{{ $cta['component'] }}', '{{ $cta['label'] }}')">{{ $cta['label'] }}</button>
+                        @else
+                        <a href="{{ $cta['route'] }}" wire:navigate class="custom-btn {{$cta['css'] ?? ''}}">{{ $cta['label'] }}</a>
+                        @endif
+                    @endif
+
+                    @if ($cta['type'] === 'button')
+                        <button type="button" class="custom-btn as-pointer {{$cta['css'] ?? ''}} {{$this->selectAll ? 'bg-aquamarine' : ''}}"
+                            wire:click="{{$cta['wireAction']}}"
+                            wire:loading.attr="disabled"
+                            wire:target="{{$cta['wireTarget']}}"
+                            wire:confirm="Do you want to continue?">{{ $cta['label'] }}</button>
+                    @endif
+
+                    @if ($cta['type'] === 'select')
+                        <select name="bulkAction" id="bulkAction">
+                            <option>Bulk Action</option>
+                            <option value="">Attach all products</option>
+                            <option value="">Delete all products</option>
+                        </select>
+                    @endif
+
+                    @if ($cta['type'] === 'dropdown')
+                        <select name="bulkAction" id="bulkAction" wire:model.live="{{$cta['wireProperty']}}"
+                            wire:change="{{$cta['wireAction']}}" class="filter-wrapper select-filter as-pointer">
+                            <option>{{$cta['label']}}</option>
+                            @forelse ($cta['options'] as $option)
+                            <option value="{{$option[$cta['optionId']]}}">{{$option[$cta['optionKey']]}}</option>
+                            @empty
+                                Add options.
+                            @endforelse
+                        </select>
+                    @endif
+                @endif
+            @endif
+        @endforeach
+    </div>
+
+    <div class="page-cta-wrapper-items mobile-cta" x-show="mobileCta" x-cloak>
         @foreach ($this->pageCta() as $key => $cta)
             @if (isset($cta['show']) && $cta['show'])
                 @if (isset($cta['type']))
