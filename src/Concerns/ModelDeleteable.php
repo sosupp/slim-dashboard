ea31777eb1138ac33e0d447d449a9ed4ccd5f6e8
Py Sosu <prospersosu@gmail.com>
2026-02-27 04:01:00 +0100

    WIP

--- src/Concerns/ModelDeleteable.php
+++ src/Concerns/ModelDeleteable.php
@@ -4,17 +4,27 @@ namespace Sosupp\SlimDashboard\Concerns;
 trait ModelDeleteable
 {
     abstract function useModel();
-    
+
+    public function deleteConditions($modelId)
+    {
+        return true;
+    }
+
     public function delete($modelId, $authorize = null)
     {
         $model = $this->useModel()::where('id', $modelId)
         ->first();
 
         // dd($model, (bool)$authorize);
+        if(!$this->deleteConditions($modelId)){
+            return;
+        }
 
         if(!(bool)$authorize){
             $this->authorize('delete', $model);
         }
+
+        $this->failAlert('Record deleted...You can refresh page for changes');
 
         return $model->delete();
 
@@ -31,6 +41,8 @@ trait ModelDeleteable
         if(!(bool)$authorize){
             $this->authorize('restore', $model);
         }
+
+        $this->successAlert('Deleted record restored...You can refresh page for changes');
 
         return $model->restore();
 
